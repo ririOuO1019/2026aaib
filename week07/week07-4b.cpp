87cdafca45b21aae30a89c9cad363ef13b6b4b1a
ririOuO1019 <gracex1210@gmail.com>
2026-04-08 11:57:23 +0800

    week07

---
+++ week07/week07-4b.cpp
@@ -0,0 +1,9 @@
+//week07-4b.cpp SOIT107_BASE_007
+//Method 2: char c
+#include <stdio.h>
+int main()
+{
+	char a, b, c, d, e;//c1, c2, c3, c4, c5;
+	scanf("%c%c%c%c%c", &a, &b, &c, &d, &e);
+	printf("%c   %c   %c   %c   %c", a, b, c, d, e);
+}
